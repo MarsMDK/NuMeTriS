e5cce0db8ce445ca687516d25a17fb3da86b9d1b
Marzio <36334966+MarsMDK@users.noreply.github.com>
2024-10-08 14:21:22 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = "2023, Marzio Di Vece"
 author = "Marzio Di Vece"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.9.'
+release = '0.1.1.'
 
 
 # -- General configuration ---------------------------------------------------
